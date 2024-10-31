21bc3a346dfa0a0bdddbc7611dc4f78aec35db24
cimontesm <cimontes@espol.edu.ec>
2024-10-31 13:47:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    int l,b,h,r, triangulo, paralelogramo, cuadrado, rectangulo, rombo, trapecio, circulo, poligono;
-    printf("Seleccione la opcion que quiera\n1.Triangulo\n2.Paralelogramo\n3.Cuadrado\n4.Rectangulo\n5.Rombo\n6.Trapecio\n7.Circulo\n7.Poligono");
+    
     return 0;
 }

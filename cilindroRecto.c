db0751c6159f403ea7e036f97444052b47d8c58e
cimontesm <cimontes@espol.edu.ec>
2024-10-31 15:41:28 -0500

    Volumenes y superficie

---
+++ cilindroRecto.c
@@ -0,0 +1,10 @@
+float radio, h, pi;
+pi= 3.1416;
+
+void calculoSuperficieCilindroLateral(radio, h){
+    float superficie = 2*pi*radio*h;
+}
+
+void calculoSuperficieCilindroTransversal(radio,h){
+    float superficie = 2*(pi*radio*h+pi*radio^2);
+}
---
+++ cuboide.c
@@ -0,0 +1,9 @@
+float a,b,c;
+
+void calculoSuperficieCuboide(a,b,c){
+    float superficie = 2*(a*b+a*c+b*c);
+}
+
+void calculoVolumenCuboide(a,b,c){
+    float volumen = a*b*c;
+}
